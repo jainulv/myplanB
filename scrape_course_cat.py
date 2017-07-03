c78afe6bd6db234f718fa1d249ebf0d8a34f489e
Jainul Vaghasia <jainulvaghasia@gmail.com>
2017-07-03 13:38:42 -0400

    Optimized code by large and added data

--- scrape_course_cat.py
+++ scrape_course_cat.py
@@ -1,49 +1,76 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
 import urllib.request
 import pandas as pd
+import json
+import io
 import time
+import concurrent.futures
 
 dept_dict={}
 course_dict={}
+
+def open(url, tag=None, lxml=False):
+    with urllib.request.urlopen(url) as url:
+        r=url.read()
+        soup=None
+        if tag:
+            only_tags=SoupStrainer(tag)
+            soup=BeautifulSoup(r, 'html.parser', parse_only=only_tags)
+        elif not lxml:
+            soup=BeautifulSoup(r, 'html5lib')
+        else:
+            soup=BeautifulSoup(r, 'lxml')
+        return soup
 
 def a_scrape(elem):
     for j in elem.find_all('a'):
         if '.html' in j['href']:
             dept_dict[j.getText()]=j['href']
 
-def open(url):
-    with urllib.request.urlopen(url) as url:
-        r=url.read()
-        return BeautifulSoup(r, 'html5lib')
-        
 def scrape(url):
     soup=open(url)
-    for uls in soup.find_all('ul'):
+    all_ul_elems=soup.find_all('ul')
+    for uls in all_ul_elems:
         if not uls.has_attr('class'):
-            for x in uls.find_all('li'):
-                for y in x.find_all('ul'):
+            li_elems=uls.find_all('li')
+            for x in li_elems:
+                ul_elems=x.find_all('ul')
+                for y in ul_elems:
                     a_scrape(y)
                 a_scrape(x)
 
 def scrape_courses(url):
-    soup=open(url)
-    for uls in soup.find_all('a'):
+    soup=open(url, 'a')
+    for uls in soup:
         if uls.has_attr('name'):
-            if uls.findChildren():
-                course_dict[uls.find('b').getText()]=uls.select('br')[0].next_sibling
+            if uls.findChildren() and len(uls.find_all('br'))>1:
+                course_dict[uls.find('b').getText()]=str(uls.select('br')[0].next_sibling)
+            else:
+                course_dict[uls.find('b').getText()]='N/A'
+
+def normalize():
+    json_data=[]
+    for k,v in course_dict.items():
+        json_data.append({'title':k,'info':v})
+    return json_data
 
 t0=time.time()
 base_url='https://www.washington.edu/students/crscat/'
 scrape(base_url)
-for i in dept_dict.values():
-    scrape_courses(base_url+i)
-courses_pd=pd.DataFrame.from_dict(course_dict, orient='index')
+print('Number of departments: ', len(dept_dict))
 
-print(courses_pd)
+URLS=(base_url+i for i in dept_dict.values())
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    future_results={executor.submit(scrape_courses, url): url for url in URLS}
+
+with io.open('../myplanB/courses.json','w') as fout:
+    json.dump(normalize(), fout)
 print('Done!')
 print('Number of courses: ', len(course_dict))
+
 t1=time.time()
 total_time=t1-t0
 print('Time taken: ', total_time)
