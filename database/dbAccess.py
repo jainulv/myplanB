90b6671c0642a563815ff5683b47544e51dc1b50
Akshit-p <akshitpatel.cs@gmail.com>
2017-07-06 15:01:33 -0700

    updated the db_name

--- database/dbAccess.py
+++ database/dbAccess.py
@@ -2,7 +2,7 @@ import couchdb
 import json
 # ask admin for username and password
 couch = couchdb.Server("http://username:password@localhost:5984")
-db = couch['course_catlog']
+db = couch['myplan_b']
 
 with open("courses.json", "r") as jsonfile:
     db_entry = json.load(jsonfile)
